e66b5987cdec9f05d5a09b23c6108d18182fa6a1
Laimne <107036125+Laimne@users.noreply.github.com>
2022-09-22 11:00:40 +0300

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './App.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Nav from './Components/Nav';
+import Home from './Components/Home';
+import MainSup from './es/Main';
+import MainCons from './es/Main';
 
 function App() {
   return (
-    <div >
-      elekktra
-    </div>
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="home" element={<Home/>}></Route>
+        <Route path="suppliers" element={<MainSup/>}></Route>
+        <Route path="consumers" element={<MainCons/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
